4d991c6be0a52a00b91add07f4edfcddfab2f7c1
Alberto Riolly <albert.r.cahyantara@gmail.com>
2020-08-27 23:56:33 +0700

    Finish setting mikro-orm

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
 import { Post } from './entities/Post';
+import mikroOrmConfig from './mikro-orm.config';
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    entities: [Post],
-    dbName: 'bible_discuss',
-    type: 'postgresql',
-    user: 'postgres',
-    password: '1234567890',
-    debug: !__prod__,
-  });
+  const orm = await MikroORM.init(mikroOrmConfig);
+  await orm.getMigrator().up();
+
+  // const postFirst = orm.em.create(Post, { title: 'This is the first post' });
+  // await orm.em.persistAndFlush(postFirst);
+
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
 };
+
 main();
